af1f44589748ee85ba37fda4f395c4f54826debd
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-29 19:03:30 -0300

    code extracted from definitely-not-dobble repo

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import "phoenix_html";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Root from "./Root";
+
+import "./style-reset.scss";
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById("definitely-not-dobble-root")
+);
