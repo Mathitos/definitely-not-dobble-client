0a241413e95895f3c5c494c3259bc90655b6e8cd
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-12-29 21:30:41 -0300

    Updates

---
+++ precache-manifest.0c6a8692a424f43ebf9fef862dbd6165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7983625a7c9f9f9376bdf5f256c13831",
+    "url": "/definitely-not-dobble-client/index.html"
+  },
+  {
+    "revision": "e85124178758ad68c61f",
+    "url": "/definitely-not-dobble-client/static/css/main.afecc2e1.chunk.css"
+  },
+  {
+    "revision": "c3a005208b2d1617ee89",
+    "url": "/definitely-not-dobble-client/static/js/2.f2a20df5.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/definitely-not-dobble-client/static/js/2.f2a20df5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e85124178758ad68c61f",
+    "url": "/definitely-not-dobble-client/static/js/main.8f4f09c0.chunk.js"
+  },
+  {
+    "revision": "2dd7e6a8632bf2cb4270",
+    "url": "/definitely-not-dobble-client/static/js/runtime-main.dc298055.js"
+  }
+]);
